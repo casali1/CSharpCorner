5e41145d538fb291c77fa173fe54b2beeb269bc4
casali1 <frank_casali@hotmail.com>
2019-08-02 11:53:11 -0600

    Add project files.

---
+++ CSharpCorner/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpCorner.Entities
+{
+    public class Book
+    {
+        [Key]
+        public Guid AuthorId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+    }
+}
